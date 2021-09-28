99616e69ac44952e7442848cab83e2ac143480d4
MardonioEng <mardoniorodrigues11@gmail.com>
2021-09-28 05:23:12 -0300

    Implementação de inserir e buscar em UsuarioController

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/usuarios', [\App\Http\Controllers\UsuarioController::class, 'buscar']);
+Route::post('/usuarios', [\App\Http\Controllers\UsuarioController::class, 'inserir']);
